d36395487982d64d6d8c98d1bacc01e2d90ea957
luketudor <Luke.Tudor@myob.com>
2018-02-16 15:03:58 +1300

    Add some error handling

--- PayslipGenerator2/Program.cs
+++ PayslipGenerator2/Program.cs
@@ -8,11 +8,21 @@ namespace PayslipGenerator2
         {
             while (true)
             {
-                var employeeDetails = Console.ReadLine();
-                var employee = new Parser().ParseEmployee(employeeDetails);
-                var payslip = new Accountant().MakePayslip(employee);
-                var formattedPayslip = new Formatter().FormatPayslip(payslip);
-                Console.WriteLine(formattedPayslip);
+                try
+                {
+                    var employeeDetails = Console.ReadLine();
+                    var employee = new Parser().ParseEmployee(employeeDetails);
+                    var payslip = new Accountant().MakePayslip(employee);
+                    var formattedPayslip = new Formatter().FormatPayslip(payslip);
+                    Console.WriteLine(formattedPayslip);
+                }
+                catch (Exception)
+                {
+                    Console.Error.WriteLine
+                    (
+                        @"Input format invalid, Expected: <First name>,<Last name>,<Annual salary>,<Super rate>%,<Payment start date>"
+                    );
+                }
             }
         }
     }
